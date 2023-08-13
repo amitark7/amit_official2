81019e32720a3838deb630073f8221431023194b
amitark7 <amitkumar1600121@gmail.com>
2023-08-13 13:59:02 +0530

    Slider Updated

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ hamburger.addEventListener('click',()=>{
         hamChild.classList.replace('fa-xmark','fa-bars');
     }
 })
+
+
+const slides=document.querySelectorAll('.slide');
+let counter=0;
+slides.forEach(
+    (slide,index) =>{
+    slide.style.left =`${index * 100}%`
+}
+)
+
+const goPrev=()=>{
+    counter--;
+    slideImage();
+}
+
+const goNext=()=>{
+    counter++;
+    if(counter>4){
+        counter=0;
+    }
+    slideImage();
+    console.log(counter)
+}
+
+const slideImage=()=>{
+    slides.forEach((slide)=>{
+        slide.style.transform=`translateX(-${counter*100}%)`
+    })
+}
+
+setInterval(()=>{
+    counter++;
+    if(counter>4){
+        counter=0;
+    }
+    slideImage();
+},1500)
